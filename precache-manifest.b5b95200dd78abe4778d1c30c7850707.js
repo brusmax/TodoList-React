b8d48a644fd26484b868fefebf1b589aa33513d0
brusmax <enrique.webgenius@gmail.com>
2022-07-08 12:59:08 -0500

    Updates

---
+++ precache-manifest.b5b95200dd78abe4778d1c30c7850707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e678ea3d726d50972e31",
+    "url": "/TodoList-React/static/css/main.20a895a4.chunk.css"
+  },
+  {
+    "revision": "e678ea3d726d50972e31",
+    "url": "/TodoList-React/static/js/main.e678ea3d.chunk.js"
+  },
+  {
+    "revision": "91d1d4c5a6814aad6413",
+    "url": "/TodoList-React/static/js/1.91d1d4c5.chunk.js"
+  },
+  {
+    "revision": "62f21f306e343e8a737e",
+    "url": "/TodoList-React/static/js/2.62f21f30.chunk.js"
+  },
+  {
+    "revision": "8ee50edf067a51294134",
+    "url": "/TodoList-React/static/js/runtime~main.8ee50edf.js"
+  },
+  {
+    "revision": "5c07152430f158712f30fa9eb9945a51",
+    "url": "/TodoList-React/index.html"
+  }
+];
