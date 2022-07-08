71356edf449446c22260b84adb52bf52e5160e57
brusmax <enrique.webgenius@gmail.com>
2022-07-07 22:36:12 -0500

    Use Context React for search and count functions

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -16,38 +16,34 @@ import { PromiseProvider } from "mongoose"
   // }
 
 function AppUI(){
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+  } = React.useContext(TodoContext);
   return(
     <React.Fragment>
       <TodoCounter />
       <TodoSearch /> 
 
-      <TodoContext.Consumer>
-        { ({ 
-          error, 
-          loading, 
-          searchedTodos, 
-          completeTodo, 
-          deleteTodo
-        }) => (
-          <TodoList>
-            {error && <p>Huy, hubo un error....</p>}
-            {loading && <p>Cargando...</p>}
-            {(!loading && !searchedTodos) && <p>Crea tu primer TODO.</p>}
-    
-            {searchedTodos.map(todo => (
-              <TodoItem
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-                onComplete={() => { completeTodo(todo.text)}}
-                onDelete={() => { deleteTodo(todo.text)}}
-              />
-            ))}
-          </TodoList>
-        )
-          
-        }
-      </TodoContext.Consumer>
+      
+      <TodoList>
+        {error && <p>Huy, hubo un error....</p>}
+        {loading && <p>Cargando...</p>}
+        {(!loading && !searchedTodos) && <p>Crea tu primer TODO.</p>}
+
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => { completeTodo(todo.text)}}
+            onDelete={() => { deleteTodo(todo.text)}}
+          />
+        ))}
+      </TodoList>
 
       <CreateTodoButton  />
     </React.Fragment> 
--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { TodoContext } from '../TodoContext'
 import './TodoCounter.css'
 
-function TodoCounter(params){
+function TodoCounter(){
+  const {totalTodos, completedTodos } = React.useContext(TodoContext)
   return(
-    <h2 className="TodoCounter">Has completado {params.completed} de {params.total} TODOs </h2>
+    <h2 className="TodoCounter">Has completado {completedTodos} de {totalTodos} TODOs </h2>
   )
 }
 
